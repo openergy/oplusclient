fa6593ea2717ab93d5bafee96ec3eab63d991a07
geoffroy.destaintot <geoffroy.destaintot@openergy.fr>
2019-09-09 16:27:04 +0200

    core chapter removed

--- vbpclient/__init__.py
+++ vbpclient/__init__.py
@@ -1,1 +1,2 @@
-from .core import OSSClient, ResourceNotFound, SimulationError
+from .high_level_client import OSSClient
+from .exceptions import ResourceNotFound, SimulationError
--- vbpclient/core/__init__.py
+++
@@ -1,2 +0,0 @@
-from .high_level_client import OSSClient
-from .exceptions import ResourceNotFound, SimulationError
