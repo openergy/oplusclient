796c74ae8049e8085767083753753bc550d28a14
Zacharie Brodard <zacharie.brodard@openergy.fr>
2020-07-03 12:12:34 +0200

    refactor geo_data_frame_to_floorplan, better color handling in floorplan

--- oplusclient/tools/floorspace.py
+++ oplusclient/tools/floorspace.py
@@ -1,8 +1,24 @@
 import os
+import random
 import json
 import uuid
 import warnings
 
+
+FLOORSPACE_COLORS = [
+      '#aa4499',
+      '#88ccee',
+      '#332288',
+      '#117733',
+      '#999933',
+      '#ddcc77',
+      '#cc6677',
+      '#882255',
+      '#44aa99',
+      '#6699cc',
+      '#661100',
+      '#aa4466',
+    ]
 
 resources_path = os.path.join(os.path.dirname(__file__), "resources")
 
@@ -21,6 +37,12 @@ class Floorplan:
     """
     def __init__(self, json_data):
         self.json_data = json_data
+        self._color_index = random.randint(0, len(FLOORSPACE_COLORS) - 1)
+
+    def get_next_color(self):
+        color = FLOORSPACE_COLORS[self._color_index]
+        self._color_index = (self._color_index + 1) % len(FLOORSPACE_COLORS)
+        return color
 
     def add_window_definition(
             self,
@@ -79,7 +101,7 @@ class Floorplan:
                 ))
                 window_i += 1
 
-    def add_story(self, name, height, color="#999933"):
+    def add_story(self, name, height, color=None):
         """
         Create an empty story.
 
@@ -98,7 +120,7 @@ class Floorplan:
             name=name,
             image_visible=True,
             height=height,
-            color=color,
+            color=color or self.get_next_color(),
             geometry=dict(
                 id=str(uuid.uuid4()),
                 vertices=[],
@@ -121,7 +143,7 @@ class Floorplan:
             zone_groups_tag_2_id=None,
             pitched_roof_id=None,
             daylighting_controls=None,
-            color="#E67E22"
+            color=None
     ):
         """
         Add a space to a story.
@@ -156,7 +178,7 @@ class Floorplan:
             zone_groups_tag_2_id=zone_groups_tag_2_id,
             pitched_roof_id=pitched_roof_id,
             daylighting_controls=daylighting_controls if daylighting_controls is not None else [],
-            color=color,
+            color=color or self.get_next_color(),
             type="space"
         ))
 
@@ -181,7 +203,7 @@ class Floorplan:
             ordered list of (x, y) coordinates of the polygon to add
         name: str
             name of the shading
-        color: str
+        color: str or None
         """
         try:
             story = [s for s in self.json_data["stories"] if s["name"] == story_name][0]
@@ -196,7 +218,7 @@ class Floorplan:
             handle=None,
             name=name,
             face_id=face_id,
-            color=color,
+            color=color or self.get_next_color(),
             type="shading"
         ))
 
@@ -436,87 +458,27 @@ class Floorplan:
         for p in polygons:
             p["polygon"] = shapely.wkt.loads(shapely.wkt.dumps(p["polygon"], rounding_precision=decimals))
 
-        # prepare points
-        points = np.array([])
-        points.shape = (0, 2)
-        for p in polygons:
-            points = np.append(points, np.array(p["polygon"].exterior.coords[:-1]), axis=0)
-        points = np.unique(points, axis=0)
-
-        # prepare edges and faces
-        edges = dict()
-        faces = []
-        for p in polygons:
-            face = dict(edges=[], edges_order=[], is_shading=p["is_shading"], name=p["name"], id=str(uuid.uuid4()))
-            for _a, _b in zip(p["polygon"].exterior.coords[:-1], p["polygon"].exterior.coords[1:]):
-                a = np.where(np.all(points == np.array(_a), axis=1))[0][0]
-                b = np.where(np.all(points == np.array(_b), axis=1))[0][0]
-                if (a, b) in edges:
-                    edge_id = edges[(a, b)]
-                    order = 1
-                elif (b, a) in edges:
-                    edge_id = edges[(b, a)]
-                    order = 0
-                else:
-                    edge_id = str(uuid.uuid4())
-                    edges[(a, b)] = edge_id
-                    order = 1
-                face["edges"].append(edge_id)
-                face["edges_order"].append(order)
-            faces.append(face)
-
         # load empty floorplan
         with open(os.path.join(resources_path, "empty_floorplan.flo")) as f:
-            floorplan = json.load(f)
+            floorplan = cls(json.load(f))
 
         # fill it with prepared data
-        fs = floorplan["stories"][0]
+        fs = floorplan.json_data["stories"][0]
         fs["name"] = story_name
         fs["height"] = story_height
-        for i, p in enumerate(points):
-            fs["geometry"]["vertices"].append(dict(
-                x=p[0],
-                y=p[1],
-                id=str(uuid.UUID(int=i)),
-                edge_ids=[]
-            ))
-        for vertices, e_id in edges.items():
-            fs["geometry"]["edges"].append(dict(
-                id=e_id,
-                vertex_ids=[str(uuid.UUID(int=vertices[0])), str(uuid.UUID(int=vertices[1]))],
-                face_ids=[]
-            ))
-            fs["geometry"]["vertices"][vertices[0]]["edge_ids"].append(e_id)
-            fs["geometry"]["vertices"][vertices[1]]["edge_ids"].append(e_id)
-        for face in faces:
-            fs["geometry"]["faces"].append(dict(
-                id=face["id"],
-                edge_ids=face["edges"],
-                edge_order=face["edges_order"]
-            ))
-            for edge in [e for e in fs["geometry"]["edges"] if e["id"] in face["edges"]]:
-                edge["face_ids"].append(face["id"])
-            if face["is_shading"]:
-                fs["shading"].append(dict(
-                    id=str(uuid.uuid4()),
-                    handle=None,
-                    name=face["name"],
-                    face_id=face["id"],
-                    color="#E8E3E5",
-                    type="shading"
-                ))
-            else:
-                fs["spaces"].append(dict(
-                    id=str(uuid.uuid4()),
-                    handle=None,
-                    name=face["name"],
-                    face_id=face["id"],
-                    zone_groups_tag_1_id=None,
-                    zone_groups_tag_2_id=None,
-                    pitched_roof_id=None,
-                    daylighting_controls=[],
-                    color="#E67E22",
-                    type="space"
-                ))
 
-        return cls(floorplan)
+        for p in polygons:
+            if p["is_shading"]:
+                floorplan.add_shading_to_story(
+                    story_name,
+                    list(p["polygon"].exterior.coords[:-1]),
+                    p["name"]
+                )
+            else:
+                floorplan.add_space_to_story(
+                    story_name,
+                    list(p["polygon"].exterior.coords[:-1]),
+                    p["name"]
+                )
+
+        return floorplan
